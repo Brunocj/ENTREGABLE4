5aa6b5c1739a951eae741a923d5b13d0b9520fd4
Daniel <a20212591@pucp.edu.pe>
2024-04-22 16:09:35 -0500

    arreglo linkeos

--- VECINO/Eventos/script.js
+++ VECINO/Eventos/script.js
@@ -25,15 +25,4 @@ function mostrarAlerta() {
     }
   }
 
-  function cerrarsesiom_1() {
-    var confirmacion = window.confirm("¿Estás seguro de que quieres cerrar sesión?");
-    if (confirmacion) {
-        // Redirigir a la página de cierre de sesión solo si el usuario acepta
-        window.location.href = "../LOGIN/login.html";
-    } else {
-        // El usuario ha cancelado la acción
-        alert("La sesión no se cerró.");
-    }
-    return false;
-}
 
